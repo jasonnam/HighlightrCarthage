9e1ca743cd38863a525669342f92348cff9624cd
Jason Nam <contact@jasonnam.com>
2016-09-01 17:36:17 +0900

    Multithreaded highlighting

--- Highlightr/Source/Classes/CodeAttributedString.swift
+++ Highlightr/Source/Classes/CodeAttributedString.swift
@@ -28,10 +28,11 @@ import Foundation
 
 public class CodeAttributedString : NSTextStorage
 {
+    private var rawString = ""
     let stringStorage = NSMutableAttributedString(string: "")
 
         /// Highlightr instace used internally for highlighting. Use this for configuring the theme.
-    public let highlightr = Highlightr()!
+    public var highlightr = Highlightr()!
     
         /// This object will be notified before and after the highlighting.
     public var highlightDelegate : HighlightDelegate?
@@ -44,11 +45,13 @@ public class CodeAttributedString : NSTextStorage
         }
     }
     
+    private var operationID = 0
+    
         /// Returns a standard String based on the current one.
     public override var string: String
     {
         get {
-            return stringStorage.string
+            return rawString
         }
     }
     
@@ -74,6 +77,7 @@ public class CodeAttributedString : NSTextStorage
     public override func replaceCharactersInRange(range: NSRange, withString str: String)
     {
         stringStorage.replaceCharactersInRange(range, withString: str)
+        rawString = stringStorage.string
         self.edited(NSTextStorageEditActions.EditedCharacters, range: range, changeInLength: (str as NSString).length - range.length)
     }
     
@@ -100,7 +104,9 @@ public class CodeAttributedString : NSTextStorage
             {
                 let string = (self.string as NSString)
                 let range = string.paragraphRangeForRange(editedRange)
-                highlight(NSMakeRange(range.location, string.length - range.location))
+                if string.substringWithRange(range) != "" {
+                    highlight(NSMakeRange(range.location, string.length - range.location))
+                }
             }
         }
     }
@@ -123,11 +129,22 @@ public class CodeAttributedString : NSTextStorage
         }
 
         
+        let ID = operationID + 1
+        self.operationID = ID
+        
         let string = (self.string as NSString)
         let line = string.substringWithRange(range)
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+            guard self.operationID == ID else {
+                return
+            }
+            
             let tmpStrg = self.highlightr.highlight(self.language!, code: line, fastRender: true)
-            dispatch_async(dispatch_get_main_queue(), {
+            dispatch_async(dispatch_get_main_queue()) {
+                guard self.operationID == ID else {
+                    return
+                }
+
                 //Checks to see if this highlighting is still valid.
                 if((range.location + range.length) > self.stringStorage.length)
                 {
@@ -151,7 +168,7 @@ public class CodeAttributedString : NSTextStorage
                 self.endEditing()
                 self.edited(NSTextStorageEditActions.EditedAttributes, range: range, changeInLength: 0)
                 self.highlightDelegate?.didHighlight?(range, success: true)
-            })
+            }
             
         }
         
